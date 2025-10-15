9ef36b2babc49fe8f314c3df9f97b2fa445e3021
rizaru22 <rizaru.2.2@gmail.com>
2025-10-15 10:56:19 +0700

    ADD kelas

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect,url_for,session
+from flask_mysqldb import MySQL,MySQLdb
+
+app=Flask(__name__)
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='db_pemilu_osis'
+
+mysql=MySQL(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/verification')
+def verification():
+    return render_template('verification.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/pemilu')
+def pemilu():
+    return render_template('pemilu/index.html')
+
+@app.route('/kelas')
+def kelas():
+    cursor=mysql.connection.cursor()
+    cursor.execute('SELECT * FROM kelas')
+    kelas=cursor.fetchall()
+    cursor.close()
+
+    return render_template('kelass/index.html', data=kelas)
+
+if __name__=='__main__':
+    app.run(debug=True)
